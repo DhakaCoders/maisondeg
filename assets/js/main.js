ee3188cb749e7a14c014e4146da64b345609fed2
Rannojit <cserannojitsarkar22@gmail.com>
2020-09-21 21:20:44 +0600

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,6 +184,15 @@ if( $('.blg-tabs').length ){
 
 
 
+$('div.fl-tabs button').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('div.fl-tabs button').removeClass('current');
+        $('.fl-tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    });
 /*----- End of Rannojit -----*/
 
 
