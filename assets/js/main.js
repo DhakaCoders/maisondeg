7e48413477be6bf40980b9f65f41bc2692f33e9f
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2020-09-24 22:42:43 +0600

    ..

--- assets/js/main.js
+++ assets/js/main.js
@@ -220,17 +220,17 @@ if( $('.blg-tabs-slider').length ){
  
 
 
-     $('.slider-single').slick({
+     $('.jd-slider-single').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false,
       fade: true,
-      asNavFor: '.slider-nav'
+      asNavFor: '.jd-slider-nav'
     });
-    $('.slider-nav').slick({
+    $('.jd-slider-nav').slick({
       slidesToShow: 3,
       slidesToScroll: 1,
-      asNavFor: '.slider-single',
+      asNavFor: '.jd-slider-single',
       dots: false,
       arrows: true,
       centerMode: true,
